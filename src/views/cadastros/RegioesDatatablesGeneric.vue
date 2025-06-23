dbea71572737010f346b774844048757ed8f7282
Emerson Euzebio <emfeuzebio@hotmail.com>
2025-06-22 23:42:33 -0300

    CRUD região

--- src/views/cadastros/RegioesDatatablesGeneric.vue
+++ src/views/cadastros/RegioesDatatablesGeneric.vue
@@ -8,7 +8,7 @@ import { useEventos } from '@/composables/useEventos';
 const { fetchEstados, estados } = useEventos();
 
 // define a Entidade Principal da View
-const entity = 'estado';
+const entity = 'regiao';
 
 // recuperas as Autorizações (abilities) do JWT
 const { can } = useAbilities();
@@ -25,53 +25,32 @@ var canPrint = can(`${entity}.print`);
 var canPrint = false;
 
 // DEBUG de todas abilities do User Logado
-console.log('Abilities carregadas:', abilities.value);
-console.log('canList:', canList); // Isso deve ser true ou false
-console.log('canShow:', canShow); // Isso deve ser true ou false
-console.log('canInsert:', canInsert); // Isso deve ser true ou false
-console.log('canUpdate:', canUpdate); // Isso deve ser true ou false
-console.log('canDelete:', canDelete); // Isso deve ser true ou false
-console.log('canPrint:', canPrint); // Isso deve ser true ou false
+// console.log('Abilities carregadas:', abilities.value);
+// console.log('canList:', canList); // Isso deve ser true ou false
+// console.log('canShow:', canShow); // Isso deve ser true ou false
+// console.log('canInsert:', canInsert); // Isso deve ser true ou false
+// console.log('canUpdate:', canUpdate); // Isso deve ser true ou false
+// console.log('canDelete:', canDelete); // Isso deve ser true ou false
+// console.log('canPrint:', canPrint); // Isso deve ser true ou false
 
 // define parâmetros das tabela de dados
 const columns = [
    { title: 'ID', data: 'id' },
    { title: 'Sigla', data: 'sigla' },
-   { title: 'Nome do Estado', data: 'descricao', class: 'fw-bold' },
-   {
-      title: 'Região',
-      data: null,
-      render(data, type, row) {
-         return '';
-      },
-   },
+   { title: 'Nome da Região', data: 'descricao', class: 'fw-bold' },
 ];
 
 // define os valores padrão dos campos do formulário
 const defaultValues = {
-   regiao_id: null,
    sigla: 'Sigla',
    descricao: 'Descrição',
 };
 
 // carrega listas de estidades da API para popular listas: <selects> os filtros
-fetchEstados();
+// fetchEstados();
 
 // filtro da página - usar quando não há filtros
 const filters = [{}]; // nessse caso sem filtros
-
-// Filtros da Página com valores fixos - filtro reativo aos dados carregados
-// const filters = computed(() => [
-//    {
-//       label: 'Estado',
-//       field: 'estado_id',
-//       type: 'select',
-//       options: estados.value.map((estado) => ({
-//          value: estado.id,
-//          label: estado.descricao,
-//       })),
-//    },
-// ]);
 </script>
 
 <template>
@@ -79,9 +58,9 @@ const filters = [{}]; // nessse caso sem filtros
    <!-- {{ eventos }} -->
 
    <GenericCrud
-      title="Cadastro de Estados "
-      description="Gerenciamento do cadastro de Estados da Federação"
-      endpoint="estado"
+      title="Cadastro de Regiões"
+      description="Gerenciamento do cadastro de Regiões do País"
+      endpoint="regiao"
       :filters="filters"
       :columns="columns"
       :defaultValues="defaultValues"
@@ -94,25 +73,7 @@ const filters = [{}]; // nessse caso sem filtros
       :canPrint="canPrint"
    >
       <template #form="{ form, errors }">
-         <!-- {{ form.value.estados }} -->
-         <!-- {{ estados }} -->
-
-         <label class="form-label fw-bold">Região do País</label>
-         <CFormSelect
-            v-model="form.value.estado_id"
-            :options="[
-               { value: '', label: 'Selecione' },
-               ...estados.map((estado) => ({
-                  value: estado.id,
-                  label: estado.descricao,
-               })),
-            ]"
-         />
-         <div class="form-error" v-if="errors.value.estado_id">
-            {{ errors.value.estado_id[0] }}
-         </div>
-
-         <label class="form-label fw-bold">Nome do Estado da Federação</label>
+         <label class="form-label fw-bold">Nome da Região do Páis</label>
          <CFormInput
             v-model="form.value.descricao"
             :class="{ 'is-invalid': errors.descricao }"
